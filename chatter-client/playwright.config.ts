c80d96c6cdd77b46cb585d63b230245eae2e7778
Connell Reffo <58084009+Crisp32@users.noreply.github.com>
2023-12-18 14:28:51 -0700

    Cleanup

--- chatter-client/playwright.config.ts
+++ chatter-client/playwright.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig, devices } from "@playwright/test"
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+// require("dotenv").config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: "./e2e",
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -27,16 +27,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    /* Base URL to use in actions like `await page.goto("/")`. */
+    baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
 
     /* Only on CI systems run the tests headless */
     headless: !!process.env.CI
@@ -45,55 +45,55 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: "chromium",
       use: {
-        ...devices['Desktop Chrome']
+        ...devices["Desktop Chrome"]
       }
     },
     {
-      name: 'firefox',
+      name: "firefox",
       use: {
-        ...devices['Desktop Firefox']
+        ...devices["Desktop Firefox"]
       }
     },
     {
-      name: 'webkit',
+      name: "webkit",
       use: {
-        ...devices['Desktop Safari']
+        ...devices["Desktop Safari"]
       }
     }
 
     /* Test against mobile viewports. */
     // {
-    //   name: 'Mobile Chrome',
+    //   name: "Mobile Chrome",
     //   use: {
-    //     ...devices['Pixel 5'],
+    //     ...devices["Pixel 5"],
     //   },
     // },
     // {
-    //   name: 'Mobile Safari',
+    //   name: "Mobile Safari",
     //   use: {
-    //     ...devices['iPhone 12'],
+    //     ...devices["iPhone 12"],
     //   },
     // },
 
     /* Test against branded browsers. */
     // {
-    //   name: 'Microsoft Edge',
+    //   name: "Microsoft Edge",
     //   use: {
-    //     channel: 'msedge',
+    //     channel: "msedge",
     //   },
     // },
     // {
-    //   name: 'Google Chrome',
+    //   name: "Google Chrome",
     //   use: {
-    //     channel: 'chrome',
+    //     channel: "chrome",
     //   },
     // },
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  // outputDir: "test-results/",
 
   /* Run your local dev server before starting the tests */
   webServer: {
@@ -102,7 +102,7 @@ export default defineConfig({
      * Use the preview server on CI for more realistic testing.
      * Playwright will re-use the local server if there is already a dev-server running.
      */
-    command: process.env.CI ? 'vite preview --port 5173' : 'vite dev',
+    command: process.env.CI ? "vite preview --port 5173" : "vite dev",
     port: 5173,
     reuseExistingServer: !process.env.CI
   }
