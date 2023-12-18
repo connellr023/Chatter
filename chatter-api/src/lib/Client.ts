372838e52b9a01fbab7c42270b864ad789ec5c83
Connell Reffo <58084009+Crisp32@users.noreply.github.com>
2023-12-17 21:51:51 -0700

    Finished documentation for the API

--- chatter-api/src/lib/Client.ts
+++ chatter-api/src/lib/Client.ts
@@ -7,18 +7,38 @@ import {StreamEvents} from "./Utility";
  */
 export default class Client {
 
+    /**
+     * The socket.io socket object this client corresponds to
+     */
     protected socket: Socket;
+
+    /**
+     * The name of this client
+     */
     protected name: string;
 
+    /**
+     * Base constructor
+     * @param socket The corresponding socket object for this client
+     * @param name The name of this client
+     */
     public constructor(socket: Socket, name: string) {
         this.socket = socket;
         this.name = name;
     }
 
+    /**
+     * Emits an event and message over this client's socket connection
+     * @param event The event to emit
+     * @param data The data to be sent
+     */
     public emit(event: StreamEvents, data: {}): void {
         this.socket.emit(event, data);
     }
 
+    /**
+     * Gets the name of this client
+     */
     public getName(): string {
         return this.name;
     }
