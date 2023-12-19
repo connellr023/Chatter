5ba481b16e37e974d4a38e18b2fb5ec769e8ecf9
Connell Reffo <58084009+Crisp32@users.noreply.github.com>
2023-12-19 14:26:01 -0700

    Fixed background scroll effect

--- chatter-client/src/components/NameLabel.vue
+++ chatter-client/src/components/NameLabel.vue
@@ -1,15 +1,34 @@
+<script setup lang="ts">
+
+function openGithub() {
+  window.open("https://github.com/connellr023", "_blank");
+}
+
+</script>
+
 <template>
-  <div id="name-label">Connell Reffo</div>
+  <div id="name-label" @click="openGithub"><span id="name-copy">&copy;</span> Connell Reffo</div>
 </template>
 
 <style scoped>
 
+span#name-copy {
+  font-size: 25px;
+}
+
 div#name-label {
-  font-size: 16px;
+  font-size: 17px;
   position: absolute;
-  bottom: 5px;
-  left: 5px;
+  bottom: 8px;
+  left: 8px;
   opacity: 0.5;
+  text-decoration: none;
+  user-select: none;
+}
+
+div#name-label:hover {
+  cursor: pointer;
+  text-decoration: underline;
 }
 
 </style>
--- chatter-client/src/components/StartConnect.vue
+++ chatter-client/src/components/StartConnect.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 
+import {onBeforeUnmount, onMounted, type Ref, ref} from "vue";
 
+const scroll = ref(0);
+let interval: number;
+
+onMounted(() => {
+  interval = window.setInterval(() => {
+    scroll.value += 0.35;
+  }, 1);
+});
+
+onBeforeUnmount(() => {
+  if (interval != null) {
+    window.clearInterval(interval);
+  }
+});
 
 </script>
 
 <template>
-  <div id="start-view-wrapper" class="background-effect">
+  <div id="start-view-wrapper" class="background-effect" :style="{'--scroll': `${scroll}px`}">
     <div id="start-connect-window">
       <img id="start-connect-logo" alt="Chatter logo" src="../assets/logo_white.png" />
     </div>
@@ -16,6 +31,7 @@
 
 .background-effect {
   --pattern-size: 60px;
+  --scroll: 0;
 
   width: 100vw;
   height: 100vh;
@@ -23,6 +39,9 @@
   background-image: linear-gradient(45deg, var(--foreground-bg-color) 25%, transparent 25%), linear-gradient(-45deg, var(--foreground-bg-color) 25%, transparent 25%), linear-gradient(45deg, transparent 75%, var(--foreground-bg-color) 75%), linear-gradient(-45deg, transparent 75%, var(--foreground-bg-color) 75%);
   background-size: var(--pattern-size) var(--pattern-size);
   background-position: 1 0, 0 calc(var(--pattern-size) / 2), calc(var(--pattern-size) / 2) calc(calc(var(--pattern-size) / 2) * -1), calc(calc(var(--pattern-size) / 2) * -1) 0;
+  background-position-x: var(--scroll);
+  background-position-y: var(--scroll);
+  transition: background-position 2s ease;
 }
 
 div#start-connect-window {
