e4f69279f9aada0bb958b3122bcc7aaab905a792
Connell Reffo <58084009+Crisp32@users.noreply.github.com>
2024-02-07 21:05:20 -0700

    Minor updates

--- chatter-client/src/lib/stream.ts
+++ chatter-client/src/lib/stream.ts
@@ -5,12 +5,13 @@
 import {io, type Socket} from "socket.io-client";
 
 export let stream: Socket;
+const inDev: boolean = (import.meta.env.MODE == "development");
 
 export function initializeStream(port: number, hostname: string): void {
-    stream = io(`http://${hostname}:${port}`, {autoConnect: false});
+    stream = io(`${inDev ? "http" : "https"}://${hostname}:${port}`, {autoConnect: false});
 }
 
-const port: number = import.meta.env.MODE == "development" ? 8000 : parseInt(window.location.port);
-const hostname: string = import.meta.env.MODE == "development" ? "localhost" : window.location.hostname;
+const port: number = inDev ? 8000 : parseInt(window.location.port);
+const hostname: string = inDev ? "localhost" : window.location.hostname;
 
 initializeStream(port, hostname);
